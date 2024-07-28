ed896fda9d928dfefa60dc8a1b37484fb20d022f
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-28 23:00:13 +0200

    Task 2 sql

--- task_2.sql
+++ task_2.sql
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS Books (
 CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
+    order_date DATE, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
     book_id INT,
     quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
