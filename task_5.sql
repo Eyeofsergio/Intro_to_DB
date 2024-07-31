f55d65b20551c6196e820b9b83b009b950bdde94
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-31 20:45:55 +0200

    Task 5sql

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 -- Insert a single row with details
 
-    INSERT INTO Customer (customer_id, customer_name, email, address)
-    VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+ INSERT INTO customer (customer_id, customer_name, email, address)
+ VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
