7ec8cb2684c98fd3daf8b03e7682dab52aa2d276
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-31 20:52:32 +0200

    Task 6sql

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,8 @@
 -- Insert multiple rows
 
-INSERT INTO Customers (customers-id, customer_name, email, address)
-VALUES
+ INSERT INTO customers (customers-id, customer_name, email, address)
+ VALUES
+ INSERT INTO customers , "124 Happiness  Ave."
     (2, 'Blessing Malik','bmalik@sandtech.com','124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com','126 Happiness Ave.'):
