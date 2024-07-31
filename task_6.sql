5c097cdda2e6a6117cbec58b6012ccf290b9a24c
Tiisetso <Tiisetsoatmash@gmail.com>
2024-07-31 20:57:32 +0200

    Task 6sql insert function added

--- task_6.sql
+++ task_6.sql
@@ -3,7 +3,7 @@
  INSERT INTO customers (customers-id, customer_name, email, address)
  VALUES
  INSERT INTO customers , "124 Happiness  Ave."
- INSERT INTO customers , "125 Happiness Ave."
+ INSERT INTO customers , "125 Happiness  Ave."
  INSERT INTO customers , "126 Happiness  Ave."
     (2, 'Blessing Malik','bmalik@sandtech.com',"124 Happiness Ave."),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', "125 Happiness Ave."),
